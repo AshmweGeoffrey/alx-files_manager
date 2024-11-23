e2ab79c6db7dfa1675eee2c0b05efad67c848d93
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2024-11-23 22:57:37 +0200

    22:57

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 import express from 'express';
-import startServer from './libs/boot';
-import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
+import injectRoutes from './routes'
 
 const server = express();
+const port = 5000;
+const host = 'localhost';
 
-injectMiddlewares(server);
-injectRoutes(server);
-startServer(server);
+// Debugging middleware
+server.use((req, res, next) => {
+  console.log('Received request:', req.method, req.url);
+  console.log('Headers:', req.headers);
+  next();
+});
 
-export default server;
+// Body parsing middleware
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+// Debugging middleware after body parsing
+server.use((req, res, next) => {
+  console.log('Parsed body:', req.body);
+  next();
+});
+
+// Apply routes
+injectRoutes(server)
+
+server.listen(port, () => {
+    console.log(`Server Running at ${port}`);
+});
